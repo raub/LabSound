077daef3f54ae24e1488319623298d418e6dc50b
Morten Andersson <morten.andersson@gmail.com>
2023-09-10 11:53:41 +0200

    add constant and moogfilter includes

--- include/LabSound/LabSound.h
+++ include/LabSound/LabSound.h
@@ -53,6 +53,8 @@
 #include "LabSound/extended/SpatializationNode.h"
 #include "LabSound/extended/SpectralMonitorNode.h"
 #include "LabSound/extended/SupersawNode.h"
+#include "LabSound/extended/ConstantNode.h"
+#include "LabSound/extended/MoogFilterNode.h"
 
 #endif
 
